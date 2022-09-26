8ad8a7d15f707ffc4c4731f087bf62fda060dfe0
zhanghaoran <2017929959@qq.com>
2022-09-26 19:49:52 +0800

    ""

--- src/main/java/com/zhr/springbootdemo01/Controller/HelloController.java
+++ src/main/java/com/zhr/springbootdemo01/Controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.zhr.springbootdemo01.Controller;
 
 import com.zhr.springbootdemo01.Dao.person;
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -23,6 +28,15 @@ public class HelloController {
         person.setAge(age);
         person.setUserName(username);
         return person;
+    }
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public Map<String,String> test(
+            @RequestHeader() Map<String ,String> map,  // 可以把请求头信息放在String中，但是必须是String,String类型的map
+            @RequestParam("inter") List<String> inter  // 可以获取多个参数
+            ) {
+        for (var i : inter)
+            System.out.println(i);
+        return map;
     }
 }
 
